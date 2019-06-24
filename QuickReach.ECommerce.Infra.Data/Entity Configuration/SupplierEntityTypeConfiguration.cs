3cdc0bfdea65a8c3edf7801775d21518bc40b9cd
myelxx <myelsuji@gmail.com>
2019-06-24 21:47:47 +0800

    --supplier test

---
+++ QuickReach.ECommerce.Infra.Data/Entity Configuration/SupplierEntityTypeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuickReach.ECommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickReach.ECommerce.Infra.Data.Entity_Configuration
+{
+    class SupplierEntityTypeConfiguration : IEntityTypeConfiguration<Supplier>
+    {
+        public void Configure(EntityTypeBuilder<Supplier> builder)
+        {
+            builder.Property(s => s.ID)
+                   .IsRequired()
+                   .ValueGeneratedOnAdd();
+
+        }
+    }
+}
---
+++ QuickReach.ECommerce.Infra.Data/Repository/SupplierRepository.cs
@@ -0,0 +1,15 @@
+﻿using QuickReach.ECommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickReach.ECommerce.Infra.Data.Repository
+{
+    public class SupplierRepository : RepositoryBase<Supplier>, IRepository<Supplier>
+    {
+        public SupplierRepository(ECommerceDbContext context) : base(context)
+        {
+
+        }
+    }
+}
