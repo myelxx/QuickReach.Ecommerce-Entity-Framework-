3f651498cf6703d8da92ec6e725d3c4cd05a0cf7
mmejidana <mmejidana@BAIDEVLP103>
2019-07-04 09:46:42 +0800

    Update. Created Cart, Cart Item, Manufacturer, Order, Order Item. Models, Controller, Repository.

---
+++ QuickReach.ECommerce.Infra.Data/Migrations/20190704014219_modify order type configuration again.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuickReach.ECommerce.Infra.Data.Migrations
+{
+    public partial class modifyordertypeconfigurationagain : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItem_Order_CartId",
+                table: "OrderItem");
+
+            migrationBuilder.RenameColumn(
+                name: "CartId",
+                table: "OrderItem",
+                newName: "OrderID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderItem_CartId",
+                table: "OrderItem",
+                newName: "IX_OrderItem_OrderID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItem_Order_OrderID",
+                table: "OrderItem",
+                column: "OrderID",
+                principalTable: "Order",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItem_Order_OrderID",
+                table: "OrderItem");
+
+            migrationBuilder.RenameColumn(
+                name: "OrderID",
+                table: "OrderItem",
+                newName: "CartId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderItem_OrderID",
+                table: "OrderItem",
+                newName: "IX_OrderItem_CartId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItem_Order_CartId",
+                table: "OrderItem",
+                column: "CartId",
+                principalTable: "Order",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
